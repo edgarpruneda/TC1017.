4acb765af366cc97b482a3d98daf94701e7c8c0c
edgarpruneda <a01630674@itesm.mx>
2015-03-02 16:20:37 -0600

    WSQ03.cpp
    Fun with numbers code

---
+++ Funwithnumbers.cpp
@@ -0,0 +1,42 @@
+
+ #include <iostream> 
+ using namespace std;
+
+  
+
+ int main() 
+ {
+ 	int a, b;
+
+
+
+	cout<<"Ingresa un numero"<< endl;
+	cin >> a;
+	cout<<"Ingresa otro numero"<< endl;
+	cin >> b;
+
+	 
+
+
+	cout << a <<"+" <<b<< " es : " << a+b << endl;
+
+	
+
+	cout << a <<"-" <<b<< " es : " << a-b << endl;
+
+	
+
+	cout << a <<"*" <<b<< " es : " << a*b << endl;
+
+	
+
+	cout << a <<"/" <<b<< " es : " << a/b << endl;
+
+	
+
+	cout << "Residuo:"  <<  a%b << endl;
+
+	
+	return 0;
+
+	}
